027f170a4fada9f897dd2cb1745faf653b7da84d
aigii1994 <a.goudaii@outlook.com>
2024-09-07 17:32:35 +0300

    Update Test.php

--- tests/Test.php
+++ tests/Test.php
@@ -1,1 +1,15 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
 
+final class UnitTest extends TestCase
+{
+    public function test1(): void
+    {
+      printf("TEST 1");
+    }
+
+    public function test2(): void
+    {
+      printf("TEST 2");
+    }
+}
